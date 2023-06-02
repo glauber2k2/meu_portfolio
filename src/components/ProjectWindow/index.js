c7598526d6c77b2142670ec2cea1517a26876c4a
glauber2k2 <devglaubermonteiro@gmail.com>
2023-06-01 21:00:45 -0300

    adding project hover

--- src/components/ProjectWindow/index.js
+++ src/components/ProjectWindow/index.js
@@ -1,9 +1,19 @@
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-
 import styles from './ProjectWindow.module.css';
 
 export default function ProjectWindow() {
+  const [hoveredItem, setHoveredItem] = useState(null);
+
+  const handleMouseEnter = (item) => {
+    setHoveredItem(item);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredItem(null);
+  };
+
   return (
     <div className={styles.container}>
       <Image src='/images/laptop.png' width={1080} height={1080} alt='' />
@@ -14,7 +24,13 @@ export default function ProjectWindow() {
         </div>
 
         <Link href='https://hamburgueria-nextjs.vercel.app/' legacyBehavior>
-          <div className={styles.inWindow}>
+          <div
+            className={`${styles.niceBurguer} ${
+              hoveredItem === 'NiceBurguer' ? styles.hovered : ''
+            }`}
+            onMouseEnter={() => handleMouseEnter('NiceBurguer')}
+            onMouseLeave={handleMouseLeave}
+          >
             <Image
               src='/images/project_image/niceburguer.jpg'
               width={1080}
@@ -22,11 +38,20 @@ export default function ProjectWindow() {
               alt=''
             />
             <h1>NiceBurguer</h1>
+            {hoveredItem === 'NiceBurguer' && (
+              <p>JavaScript, Next, React, Html, Css</p>
+            )}
           </div>
         </Link>
 
         <Link href='https://e-commerce-six-smoky.vercel.app/' legacyBehavior>
-          <div className={styles.inWindow}>
+          <div
+            className={`${styles.commerce} ${
+              hoveredItem === 'E-commerce' ? styles.hovered : ''
+            }`}
+            onMouseEnter={() => handleMouseEnter('E-commerce')}
+            onMouseLeave={handleMouseLeave}
+          >
             <Image
               src='/images/project_image/ecommerce.jpg'
               width={1080}
@@ -34,11 +59,20 @@ export default function ProjectWindow() {
               alt=''
             />
             <h1>E-commerce</h1>
+            {hoveredItem === 'E-commerce' && (
+              <p>Typescript, Next, React, Html, Css</p>
+            )}
           </div>
         </Link>
 
         <Link href='https://trinity-sepia.vercel.app/' legacyBehavior>
-          <div className={styles.inWindow}>
+          <div
+            className={`${styles.trinity} ${
+              hoveredItem === 'Trinity' ? styles.hovered : ''
+            }`}
+            onMouseEnter={() => handleMouseEnter('Trinity')}
+            onMouseLeave={handleMouseLeave}
+          >
             <Image
               src='/images/project_image/trinity.jpg'
               width={1080}
@@ -46,11 +80,20 @@ export default function ProjectWindow() {
               alt=''
             />
             <h1>Trinity</h1>
+            {hoveredItem === 'Trinity' && (
+              <p>JavaScript, Next, React, Html, Css</p>
+            )}
           </div>
         </Link>
 
         <Link href='https://redesocial-seven.vercel.app/' legacyBehavior>
-          <div className={styles.inWindow}>
+          <div
+            className={`${styles.redesocial} ${
+              hoveredItem === 'Rede Social' ? styles.hovered : ''
+            }`}
+            onMouseEnter={() => handleMouseEnter('Rede Social')}
+            onMouseLeave={handleMouseLeave}
+          >
             <Image
               src='/images/project_image/rede.jpg'
               width={1080}
@@ -58,6 +101,9 @@ export default function ProjectWindow() {
               alt=''
             />
             <h1>Rede social</h1>
+            {hoveredItem === 'Rede Social' && (
+              <p>Typescript, React, Html, Css</p>
+            )}
           </div>
         </Link>
       </div>
